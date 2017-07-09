c608901d11319bb6ea23aae6e5d3b07de3c3b7ba
melick <lyle@melick.net>
2017-07-09 13:08:17 -0400

    Latest Version: ----------------------------------------------------------------------- $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$ $WCDATE=%Y-%b-%d %I:%M:%S%p$
    Change Log:
    -----------------------------------------------------------------------
    2017 July 07 - LOMelick
                 - set up on Destiny, cleaned up some of the backend/build structure
                   updated README.md
    
    2017 June 28 - LOMelick
                 - Added TwitterBot.pl to post messages to twitter account
    
    2016 June 16 - LOMelick
                 - moved to GitHub
    
    2015 March 23 - LOMelick
                  - Created SVN Repository

--- mkCodeBook.pl
+++ mkCodeBook.pl
@@ -195,17 +195,6 @@ for (my $day=$num_days; $day >= 1; $day--) {
     #SET key = UNHEX(SHA2('" . $network . "',512));
     my $query = "INSERT INTO `CodeBook` (`CodeBook`, `date`, `Umkehrwalze`, `Walzenlage1`, `Walzenlage2`, `Walzenlage3`, `Walzenlage4`, `Ringstellung`, `Grundstellung`, `Steckerverbindungen`, `Kenngruppen`, `Revision`, `LastUpdate`) VALUES (
         AES_ENCRYPT(" . $network . ",key),
-        '" . $date . "',
-        AES_ENCRYPT(" . $Umkehrwalze . ",key),
-        AES_ENCRYPT(" . $Walzenlage[0] . ",key),
-        AES_ENCRYPT(" . $Walzenlage[1] . ",key),
-        AES_ENCRYPT(" . $Walzenlage[2] . ",key),
-        AES_ENCRYPT(" . $Walzenlage[3] . ",key),
-        AES_ENCRYPT(" . $Ringstellung . ",key),
-        AES_ENCRYPT(" . $Grundstellung . ",key),
-        AES_ENCRYPT(" . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . " " . pop @Steckerverbindungen . ",key),
-        AES_ENCRYPT(" . $Kenngruppen . ",key),
-        AES_ENCRYPT(" . $Revision . ",key),
         NOW()
     )";
     printf "query: [%s]\n", $query if $debug;
