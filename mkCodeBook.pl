4299486874e52e42ffa09e4887f2a4291e61f813
melick <lyle@melick.net>
2017-07-09 14:20:02 -0400

    Latest Version: ----------------------------------------------------------------------- $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$ $WCDATE=%Y-%b-%d %I:%M:%S%p$
    Change Log:
    -----------------------------------------------------------------------
    2017 July 07 - LOMelick
                 - set up on Destiny, cleaned up some of the backend/build structure
                   updated README.md
    
    2017 June 28 - LOMelick
                 - Added TwitterBot.pl to post messages to twitter account
    
    2016 June 16 - LOMelick
                 - moved to GitHub
    
    2015 March 23 - LOMelick
                  - Created SVN Repository

--- mkCodeBook.pl
+++ mkCodeBook.pl
@@ -31,6 +31,7 @@ GetOptions ("debug"   => \$debug,     # flag
             "start=s" => \$StartDate, # string
             "verbose" => \$verbose)   # flag
 or die("Error in command line arguments\n");
+defined($StartDate) or die("Missing required StartDate [-s or --start]");
 
 
 # ----- handle the date variable
