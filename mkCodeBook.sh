274dc8afc96b693ae3ad2dce9fe32a3bdb693f98
melick <lyle@melick.net>
2017-07-13 08:34:54 -0400

    Latest Version: ----------------------------------------------------------------------- $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$ $WCDATE=%Y-%b-%d %I:%M:%S%p$
    Change Log:
    -----------------------------------------------------------------------
    2017 July 13 - LOMelick
                 - added the ability to pass the network to mkCodeBook.pl
                 - updated mkCodeBook.sh to generate Red Stallion, Pioneer
                   and Viking patrol codebooks.
                 - added ability to upload codebooks to my Wordpress blog's
                   media page vi https://metacpan.org/release/WP-API
    
    2017 July 07 - LOMelick
                 - set up on Destiny, cleaned up some of the build structure
                   updated README.md
    
    2017 June 28 - LOMelick
                 - Added TwitterBot.pl to post messages to twitter account
    
    2016 June 16 - LOMelick
                 - moved to GitHub
    
    2015 March 23 - LOMelick
                  - Created SVN Repository

--- mkCodeBook.sh
+++ mkCodeBook.sh
@@ -14,8 +14,9 @@ today=`date '+%Y-%m'`;
 echo $today
 
 # ----- this will create the 'Red Stallion' patrol codebook.
-/usr/bin/perl /home/melick/enigma/mkCodeBook.pl --patrol 'Red Stallion' --start '$today-01' > /users/melick/enigma/CodeBook-RS-$today.txt
-cat /users/melick/enigma/CodeBook-RS-$today.txt | /usr/bin/unix2dos | /usr/bin/a2ps --chars-per-line=104 --columns=1 --landscape --no-header --output=- | /usr/bin/ps2pdf - /users/melick/enigma/CodeBook-RS-$today.pdf
+/usr/bin/perl /home/melick/enigma/mkCodeBook.pl --patrol 'Red Stallion' --start '$today-01'
+#> /users/melick/enigma/CodeBook-RS-$today.txt
+#cat /users/melick/enigma/CodeBook-RS-$today.txt | /usr/bin/unix2dos | /usr/bin/a2ps --chars-per-line=104 --columns=1 --landscape --no-header --output=- | /usr/bin/ps2pdf - /users/melick/enigma/CodeBook-RS-$today.pdf
 
 ## ----- this will create the 'Viking' patrol codebook.
 #/usr/bin/perl /home/melick/enigma/mkCodeBook.pl --patrol 'Viking' --start '$today-01' > /users/melick/enigma/CodeBook-V-$today.txt
