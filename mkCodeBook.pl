05444c677a5d945e423d92d18a420fbbf1853010
melick <lyle@melick.net>
2017-07-12 22:24:17 -0400

    Latest Version: ----------------------------------------------------------------------- $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$ $WCDATE=%Y-%b-%d %I:%M:%S%p$
    Change Log:
    -----------------------------------------------------------------------
    2017 July 07 - LOMelick
                 - set up on Destiny, cleaned up some of the backend/build structure
                   updated README.md
    
    2017 June 28 - LOMelick
                 - Added TwitterBot.pl to post messages to twitter account
    
    2016 June 16 - LOMelick
                 - moved to GitHub
    
    2015 March 23 - LOMelick
                  - Created SVN Repository

--- mkCodeBook.pl
+++ mkCodeBook.pl
@@ -180,10 +180,10 @@ for (my $day=$num_days; $day >= 1; $day--) {
     for (my $s=0; $s < $max_plug; $s++) {
         if ($s %2) {
             # odd
-            $Steckerverbindungen = join('', @negnudnibrevrekcetS[$n]);
+            $Steckerverbindungen = join('', @negnudnibrevrekcetS[$s]);
         } else {
             # even
-            $Steckerverbindungen = join(' ', @negnudnibrevrekcetS[$n]);
+            $Steckerverbindungen = join(' ', @negnudnibrevrekcetS[$s]);
         }
         printf " - [%s]\n", $Steckerverbindungen if $debug;
     }
