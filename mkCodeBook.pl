abbdb58b3f0af0f30b75e0943b85bf017482c651
melick <lyle@melick.net>
2017-07-12 21:07:10 -0400

    Latest Version: ----------------------------------------------------------------------- $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$ $WCDATE=%Y-%b-%d %I:%M:%S%p$
    Change Log:
    -----------------------------------------------------------------------
    2017 July 07 - LOMelick
                 - set up on Destiny, cleaned up some of the backend/build structure
                   updated README.md
    
    2017 June 28 - LOMelick
                 - Added TwitterBot.pl to post messages to twitter account
    
    2016 June 16 - LOMelick
                 - moved to GitHub
    
    2015 March 23 - LOMelick
                  - Created SVN Repository

--- mkCodeBook.pl
+++ mkCodeBook.pl
@@ -170,24 +170,25 @@ for (my $day=$num_days; $day >= 1; $day--) {
     # Pick cards from @deck
     my @negnudnibrevrekcetS = @Plugs[ @pick_plug_indexes ];
 
-    # I've got space in the output for 24, but might not use them all if $max_plug < 24.  Pushe the unneeded ones off the back end of the truck.
+    # I've got space in the output for 24, but might not use them all if $max_plug < 24.  Push the unneeded ones off the back end of the truck.
     for (my $s=$max_plug; $s < 24; $s++) {
-        if ($s %2) {
-            # odd
-            push @negnudnibrevrekcetS, '';
-        } else {
-            # even
             push @negnudnibrevrekcetS, ' ';
-        }
     }
 
     # what string of perls did I end up with
+    #my $Steckerverbindungen = join('.', @negnudnibrevrekcetS[0..$max_plug]);
+    my $Steckerverbindungen = '';
     foreach my $n (@negnudnibrevrekcetS) {
+        if ($s %2) {
+            # odd
+            $Steckerverbindungen = join('', @negnudnibrevrekcetS[$n]);
+        } else {
+            # even
+            $Steckerverbindungen = join(' ', @negnudnibrevrekcetS[$n]);
+        }
         printf " - [%s]\n", $n if $debug;
     };
-
-    my $Steckerverbindungen = join('.', @negnudnibrevrekcetS[0..$max_plug]);
-    printf "Steckerverbindungen:%s\n", $Steckerverbindungen if $debug;
+    printf "Final Steckerverbindungen:%s\n", $Steckerverbindungen if $debug;
 
 
     # ----------------------------------------------------------------------
