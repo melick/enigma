312a2e781e69979291e33cd4b19145d9f0b04017
melick <lyle@melick.net>
2017-07-13 09:54:44 -0400

    Latest Version: ----------------------------------------------------------------------- $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$ $WCDATE=%Y-%b-%d %I:%M:%S%p$
    Change Log:
    -----------------------------------------------------------------------
    2017 July 13 - LOMelick
                 - added the ability to pass the network to mkCodeBook.pl
                 - updated mkCodeBook.sh to generate Red Stallion, Pioneer
                   and Viking patrol codebooks.
                 - added ability to upload codebooks to my Wordpress blog's
                   media page vi https://metacpan.org/release/WP-API
    
    2017 July 07 - LOMelick
                 - set up on Destiny, cleaned up some of the build structure
                   updated README.md
    
    2017 June 28 - LOMelick
                 - Added TwitterBot.pl to post messages to twitter account
    
    2016 June 16 - LOMelick
                 - moved to GitHub
    
    2015 March 23 - LOMelick
                  - Created SVN Repository

--- mkCodeBook.sh
+++ mkCodeBook.sh
@@ -15,12 +15,12 @@ echo ${today}
 
 # ----- this will create the 'Red Stallion' patrol codebook.
 /usr/bin/perl /home/melick/enigma/mkCodeBook.pl --patrol 'Red Stallion' --start ${today}-01 > /home/melick/enigma/CodeBook-RS-${today}.txt
-#cat /home/melick/enigma/CodeBook-RS-${today}.txt | /usr/bin/unix2dos | /usr/bin/a2ps --chars-per-line=104 --columns=1 --landscape --no-header --output=- | /usr/bin/ps2pdf - /home/melick/enigma/CodeBook-RS-${today}.pdf
+cat /home/melick/enigma/CodeBook-RS-${today}.txt | /usr/bin/unix2dos | /usr/bin/a2ps --chars-per-line=104 --columns=1 --landscape --no-header --output=- | /usr/bin/ps2pdf - /home/melick/enigma/CodeBook-RS-${today}.pdf
 
-## ----- this will create the 'Viking' patrol codebook.
-#/usr/bin/perl /home/melick/enigma/mkCodeBook.pl --patrol 'Viking' --start ${today}-01  > /home/melick/enigma/CodeBook-V-${today}.txt
-#cat /home/melick/enigma/CodeBook-V-${today}.txt | /usr/bin/unix2dos | /usr/bin/a2ps --chars-per-line=104 --columns=1 --landscape --no-header --output=- | /usr/bin/ps2pdf - /home/melick/enigma/CodeBook-V-${today}.pdf
+# ----- this will create the 'Viking' patrol codebook.
+/usr/bin/perl /home/melick/enigma/mkCodeBook.pl --patrol 'Viking' --start ${today}-01  > /home/melick/enigma/CodeBook-V-${today}.txt
+cat /home/melick/enigma/CodeBook-V-${today}.txt | /usr/bin/unix2dos | /usr/bin/a2ps --chars-per-line=104 --columns=1 --landscape --no-header --output=- | /usr/bin/ps2pdf - /home/melick/enigma/CodeBook-V-${today}.pdf
 
-## ----- this will create the 'Pioneer' patrol codebook.
-#/usr/bin/perl /home/melick/enigma/mkCodeBook.pl --patrol 'Viking' --start ${today}-01  > /home/melick/enigma/CodeBook-P-${today}.txt
-#cat /home/melick/enigma/CodeBook-P-${today}.txt | /usr/bin/unix2dos | /usr/bin/a2ps --chars-per-line=104 --columns=1 --landscape --no-header --output=- | /usr/bin/ps2pdf - /home/melick/enigma/CodeBook-P-${today}.pdf
+# ----- this will create the 'Pioneer' patrol codebook.
+/usr/bin/perl /home/melick/enigma/mkCodeBook.pl --patrol 'Viking' --start ${today}-01  > /home/melick/enigma/CodeBook-P-${today}.txt
+cat /home/melick/enigma/CodeBook-P-${today}.txt | /usr/bin/unix2dos | /usr/bin/a2ps --chars-per-line=104 --columns=1 --landscape --no-header --output=- | /usr/bin/ps2pdf - /home/melick/enigma/CodeBook-P-${today}.pdf
