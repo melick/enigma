8261d4d5e1e9f5c34760252cb9aa47b5dbda3cf6
melick <lyle@melick.net>
2017-07-13 07:23:27 -0400

    Latest Version: ----------------------------------------------------------------------- $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$ $WCDATE=%Y-%b-%d %I:%M:%S%p$
    Change Log:
    -----------------------------------------------------------------------
    2017 July 13 - LOMelick
                 - added the ability to pass the network to mkCodeBook.pl
                 - updated mkCodeBook.sh to generate Red Stallion, Pioneer
                   and Viking patrol codebooks.
                 - added ability to upload codebooks to my Wordpress blog's
                   media page vi https://metacpan.org/release/WP-API
    
    2017 July 07 - LOMelick
                 - set up on Destiny, cleaned up some of the build structure
                   updated README.md
    
    2017 June 28 - LOMelick
                 - Added TwitterBot.pl to post messages to twitter account
    
    2016 June 16 - LOMelick
                 - moved to GitHub
    
    2015 March 23 - LOMelick
                  - Created SVN Repository

--- mkCodeBook.pl
+++ mkCodeBook.pl
@@ -36,6 +36,7 @@ GetOptions ("debug"   => \$debug,     # flag
             "verbose" => \$verbose)   # flag
 or die("Error in command line arguments\n");
 die("StartDate is not defined.\n") if ( ! $StartDate );
+die("Patrol is not defined.\n") if ( ! $Patrol );
 
 
 # ----- handle the date variable
@@ -52,10 +53,6 @@ use lib '/home/melick/perl5/lib/perl5';
 use Melick::dbLib qw(connection ckObject );
 my $dbh = &connection($which_db);
 printf "dbh: [%s]\n", $dbh if $debug;
-
-
-# ----- basic info
-my $network = 'Red Stallion';
 
 
 # ----------------------------------------------------------------------
@@ -76,9 +73,8 @@ foreach my $n (@letters) {
 # ----------------------------------------------------------------------
 # ----- print header
 # ----------------------------------------------------------------------
-#$network = ' ' . $network;
 $a = Text::Banner->new;
-$a->set($network);
+$a->set($Patrol);
 $a->size(1);
 $a->fill('*');
 $a->rotate('h');
@@ -207,7 +203,7 @@ for (my $day=$num_days; $day >= 1; $day--) {
     # ----------------------------------------------------------------------
     # remove old entry
     # ----------------------------------------------------------------------
-    my $delete_query = "DELETE FROM `CodeBook` WHERE `date` = '" . $date . "' AND AES_DECRYPT(`CodeBook`,UNHEX(SHA2('" . $network . "',512))) = '" . $network . "';";
+    my $delete_query = "DELETE FROM `CodeBook` WHERE `date` = '" . $date . "' AND AES_DECRYPT(`Patrol`,UNHEX(SHA2('" . $Patrol . "',512))) = '" . $Patrol . "';";
     printf "delete_query: [%s]\n", $delete_query if $debug;
     my $sth_d = $dbh->prepare($delete_query);
     $sth_d->execute() or die "Can't execute SQL statement: $DBI::errstr\n";
@@ -217,19 +213,19 @@ for (my $day=$num_days; $day >= 1; $day--) {
     # store in database -- http://thinkdiff.net/mysql/encrypt-mysql-data-using-aes-techniques/
     # ----------------------------------------------------------------------
     my $return_value = 0;
-    my $query = "INSERT INTO `CodeBook` (`CodeBook`, `date`, `Umkehrwalze`, `Walzenlage1`, `Walzenlage2`, `Walzenlage3`, `Walzenlage4`, `Ringstellung`, `Grundstellung`, `Steckerverbindungen`, `Kenngruppen`, `Revision`, `LastUpdate`) VALUES (
-            AES_ENCRYPT('" . $network . "', UNHEX(SHA2('" . $network . "',512))),
+    my $query = "INSERT INTO `CodeBook` (`Patrol`, `date`, `Umkehrwalze`, `Walzenlage1`, `Walzenlage2`, `Walzenlage3`, `Walzenlage4`, `Ringstellung`, `Grundstellung`, `Steckerverbindungen`, `Kenngruppen`, `Revision`, `LastUpdate`) VALUES (
+            AES_ENCRYPT('" . $Patrol . "', UNHEX(SHA2('" . $Patrol . "',512))),
             '" . $date . "',
-            AES_ENCRYPT('" . $Umkehrwalze . "', UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Walzenlage[0] . "', UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Walzenlage[1] . "', UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Walzenlage[2] . "', UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Walzenlage[3] . "', UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Ringstellung . "', UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Grundstellung . "',UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Steckerverbindungen . "',UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Kenngruppen . "',UNHEX(SHA2('" . $network . "',512))),
-            AES_ENCRYPT('" . $Revision . "',UNHEX(SHA2('" . $network . "',512))),
+            AES_ENCRYPT('" . $Umkehrwalze . "', UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Walzenlage[0] . "', UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Walzenlage[1] . "', UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Walzenlage[2] . "', UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Walzenlage[3] . "', UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Ringstellung . "', UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Grundstellung . "',UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Steckerverbindungen . "',UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Kenngruppen . "',UNHEX(SHA2('" . $Patrol . "',512))),
+            AES_ENCRYPT('" . $Revision . "',UNHEX(SHA2('" . $Patrol . "',512))),
             NOW());";
     printf "query: [%s]\n", $query if $debug;
     my $sth = $dbh->prepare($query);
@@ -249,9 +245,30 @@ for (my $day=$num_days; $day >= 1; $day--) {
 
 }
 
-printf "-----------+---+---------------------+-----+-----+----------------------------------------+-------------\n";
+printf "-----------+---+---------------------+-----+-----+-------------------------------------+----------------\n";
 
 $dbh->disconnect;
+
+
+=begin GHOSTCODE
+# ----------------------------------------------------------------------
+# upload to wordpress blog
+# ----------------------------------------------------------------------
+use File::Slurp qw( read_file );
+ 
+my $content = read_file('path/to/file.jpg');
+ 
+my $media = $api->media()->create(
+    name      => 'foo.jpg',
+    type      => 'image/jpeg',
+    bits      => $content,
+    overwrite => 1,
+);
+ 
+print $media->date_created_gmt()->date();
+=end GHOSTCODE
+=cut
+
 
 
 # ----- HC SVNT DRACONES -----
