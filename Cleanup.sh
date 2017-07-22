b6aed36d9660fc06fdd039e0bfaf6e215655a63c
melick <lyle@melick.net>
2017-07-22 12:28:23 -0400

    Latest Version: ----------------------------------------------------------------------- $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$ $WCDATE=%Y-%b-%d %I:%M:%S%p$
    Change Log:
    -----------------------------------------------------------------------
    2017 July 13 - LOMelick
                 - added the ability to pass the network to mkCodeBook.pl
                 - updated mkCodeBook.sh to generate Red Stallion, Pioneer
                   and Viking patrol codebooks.
                 - added ability to upload codebooks to my Wordpress blog's
                   media page vi https://metacpan.org/release/WP-API
    
    2017 July 07 - LOMelick
                 - set up on Destiny, cleaned up some of the build structure
                   updated README.md
    
    2017 June 28 - LOMelick
                 - Added TwitterBot.pl to post messages to twitter account
    
    2016 June 16 - LOMelick
                 - moved to GitHub
    
    2015 March 23 - LOMelick
                  - Created SVN Repository

---
+++ Cleanup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# =====================================================================
+# Author:      Lyle Melick - Red Stallion Patrol
+# Create date: 2017 July 22
+# Description: converts files to unix line endings
+#
+# $WCMIXED?[$WCRANGE$]:v$WCREV$$ $WCMODS?with local mods:$
+# $WCDATE=%Y-%b-%d %I:%M:%S%p$
+# =====================================================================
+
+dos2unix /home/melick/enigma/*.pl
+dos2unix /home/melick/enigma/*.sh
+dos2unix /home/melick/enigma/*.svn
+dos2unix /home/melick/enigma/*.yaml
+dos2unix /home/melick/enigma/*.txt
+dos2unix /home/melick/enigma/*.md
