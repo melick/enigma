d7beecdc9b49f0e526df8fbbfb3e1529e08745bc
melick <lyle@melick.net>
2017-10-09 10:19:31 -0400

    write data to files, add additional information
    Signed-off-by: melick <lyle@melick.net>

--- EnigmaWeather.py
+++ EnigmaWeather.py
@@ -1,20 +1,44 @@
 import pyowm
 import json
+import csv
+from time import gmtime, strftime
 
-owm = pyowm.OWM('cf7844bf5d23112eb26c6feb5cb2d905')
+'''
+Connect to Open Weather Map (http://bigl.es/using-python-to-get-weather-data/), making sure it's online first
+- pull down weather reading for Sunbury, OH - USA
+  - pull location name from observation
+  - pull reception time of observation
+  - break out parts of observation we're interested in
+- get current timestamp
+- write to csv file
+'''
 
-# http://bigl.es/using-python-to-get-weather-data/
+
+DEFAULT_API_KEY = "cf7844bf5d23112eb26c6feb5cb2d905"
+city_id = 5173412
+
+
+owm = pyowm.OWM(DEFAULT_API_KEY)
 online = owm.is_API_online()
 if online:
 
-  # print "OWM was online (%s)" %(online)
+    print "OWM was online (%s)" %(online)
 
-    # ----- make the observation
-    observation = owm.weather_at_place("Sunbury,OH")
+    '''Make the observation'''
+    observation = owm.weather_at_id(city_id)
     w = observation.get_weather()
 
+    '''Get the location identification'''
+    l = observation.get_location()
+    name    = l.get_name()
+    # lon     = l.get_lon()
+    # lat     = l.get_lat()
+    # city_id = l.get_ID()
 
-    # ----- pull the bits we're interested in
+    '''Determine when was the observation taken'''
+    received = observation.get_reception_time(timeformat='iso') 
+
+    '''pull the bits we're interested in'''
     time         = w.get_reference_time(timeformat='iso')             # ...or in ISO8601 => '2013-08-30 14:16:46+00'
     det_status   = w.get_detailed_status()                            # Get detailed weather status => 'Broken clouds'
     clouds       = w.get_clouds()                                     # Get cloud coverage
@@ -29,40 +53,19 @@ if online:
     rain         = w.get_rain().get('3h', '')                         # Get rain volume only offers 3h I think
     snow         = w.get_snow().get('3h', '')                         # Get snow volume only offers 3h I think
 
+    '''Pull current system time'''
+    ts           = strftime("%Y-%m-%d %H:%M:%S", gmtime())            # Time script was run
 
-    # ----- print them out to the console
-  # print(w)
-    print(det_status)
-    print "%s%% cloud cov" %(clouds)
-    print "wind %s" %(speed)
-    print "dir %s" %(direction)
-    print "pressure %s" %(pressure)
-    print "temperature %s" %(temperature)
-    print "%s%%rh" %(humidity)
-    if rain == "":
-        pass
-    else:
-        print "%s rain volume last 3h" %(rain)
-    if snow == "":
-        pass
-    else:
-        print "%s snow volumen last 3h" %(snow)
-
-
-    # ----- get the location identification
-    l = observation.get_location()
-  # name = l.get_name()
-  # lon  = l.get_lon()
-  # lat  = l.get_lat()
-    id   = l.get_ID()
-
-
-    # ----- when was the observation taken?
-    received = observation.get_reception_time(timeformat='iso') 
-
-
-    # ----- print it out as well
-    print "for %s at %s, received %s." # id time received
+    '''Open output file in apend mode, write data'''
+    with open('datafile.csv', 'a') as datafile:
+        csvwriter = csv.writer(datafile, delimiter=',', dialect='excel',
+                               quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+        csvwriter.writerow([name, received, time, det_status, clouds, speed, direction, pressure, temperature, humidity, rain, snow, ts])
+    '''put your toys away, little Johnny'''
+    datafile.close()
 
 else:
     print "OWM was offline (%s)" %(online)
+
+
+'''HC SVNT DRACONES'''
