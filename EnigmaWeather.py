c623578bff907f106f8a8d0881821bf29c68629d
melick <lyle@melick.net>
2017-10-09 11:09:00 -0400

    Add all components of weather observation
    Signed-off-by: melick <lyle@melick.net>

--- EnigmaWeather.py
+++ EnigmaWeather.py
@@ -11,6 +11,7 @@ Connect to Open Weather Map (http://bigl.es/using-python-to-get-weather-data/), 
   - break out parts of observation we're interested in
 - get current timestamp
 - write to csv file
+/usr/bin/python /home/melick/enigma/EnigmaWeather.py
 '''
 
 
@@ -38,20 +39,32 @@ if online:
     '''Determine when was the observation taken'''
     received = observation.get_reception_time(timeformat='iso') 
 
-    '''pull the bits we're interested in'''
+    '''
+    pull the bits we're interested in
+    https://pyowm.readthedocs.io/en/latest/pyowm.webapi25.html#
+    '''
     time         = w.get_reference_time(timeformat='iso')             # ...or in ISO8601 => '2013-08-30 14:16:46+00'
-    det_status   = w.get_detailed_status()                            # Get detailed weather status => 'Broken clouds'
-    clouds       = w.get_clouds()                                     # Get cloud coverage
+    sunset       = w.get_sunset_time()                                # (int or None) – GMT UNIX time of sunset or None on polar days
+    sunrise      = w.get_sunrise_time()                               # (int or None) – GMT UNIX time of sunrise or None on polar nights
+    clouds       = w.get_clouds()                                     # Get cloud coverage percentage
+    rain         = w.get_rain().get('3h', '')                         # Get rain volume only offers 3h I think
+    snow         = w.get_snow().get('3h', '')                         # Get snow volume only offers 3h I think
     speed        = w.get_wind()['speed']                              # wind info.
     direction    = w.get_wind()['deg']                                # "
+    humidity     = w.get_humidity()
     pressure     = w.get_pressure()['press']                          # Get atmospheric pressure
     #              w.get_temperature()                                # Get temperature in Kelvin
     #              w.get_temperature(unit='celsius')                  # ... or in Celsius degs
     temperature  = w.get_temperature('fahrenheit')['temp']            # ... or in Fahrenheit degs
     #                                                                 # temp_min, temp, temp_max
-    humidity     = w.get_humidity()
-    rain         = w.get_rain().get('3h', '')                         # Get rain volume only offers 3h I think
-    snow         = w.get_snow().get('3h', '')                         # Get snow volume only offers 3h I think
+    status       = w.get_status                                       # (Unicode) – short weather status
+    det_status   = w.get_detailed_status()                            # Get detailed weather status => 'Broken clouds'
+    code         = w.get_weather_code                                 # (int) – OWM weather condition code    - https://openweathermap.org/weather-conditions
+    icon         = w.get_weather_icon_name                            # (Unicode) – weather-related icon name - https://openweathermap.org/weather-conditions
+    viz          = w.get_visibility_distance                          # (float) – visibility distance
+    dew          = w.get_dewpoint                                     # (float) – dewpoint
+    humidex      = w.get_humidex                                      # (float) – Canadian humidex
+    heat         = w.get_heat_index                                   # (float) – heat index
 
     '''Pull current system time'''
     ts           = strftime("%Y-%m-%d %H:%M:%S", gmtime())            # Time script was run
@@ -60,7 +73,7 @@ if online:
     with open('datafile.csv', 'a') as datafile:
         csvwriter = csv.writer(datafile, delimiter=',', dialect='excel',
                                quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-        csvwriter.writerow([name, received, time, det_status, clouds, speed, direction, pressure, temperature, humidity, rain, snow, ts])
+        csvwriter.writerow([city_id, name, received, time, sunset, sunrise, clouds, rain, snow, speed, direction, humidity, pressure, temperature, status, det_status, code, icon, viz, dew, humidex, heat, ts])
     '''put your toys away, little Johnny'''
     datafile.close()
 
